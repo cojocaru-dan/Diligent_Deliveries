23c7d4e13ea90efa427fac258e7df83d7931624a
appuser <appuser@journey2-86bff4fbf7-ljkb2>
2023-07-26 15:56:47 +0000

    starter project

---
+++ Codecool.DiligentDeliveries/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Codecool.DiligentDeliveries;
+
+class Program
+{
+    private static Random Random = new();
+
+    private static string GetRandomStreetAddress() =>
+        $"Random Street{Random.Next(100, 1000)} Number {Random.Next(0, 100)}";
+    
+
+    public static void Main(string[] args)
+    {
+        const int packages = 10;
+        const int maximumReattempts = 5;
+    }
+}
---
+++ Codecool.DiligentDeliveries/Rider.cs
@@ -0,0 +1,37 @@
+﻿namespace Codecool.DiligentDeliveries;
+
+public class Rider
+{
+    public int Id { get; }
+    public string Name => $"Rider{Id}";
+
+    public Rider(int id, int reattemptPerAddress, int maximumReattempts)
+    {
+        //Initialize members
+    }
+
+    public void AddParcels(List<Parcel> parcels)
+    {
+        //Implement
+    }
+
+    public void StartRoutine()
+    {
+        //Implement
+    }
+
+    private bool Reattempt(Parcel parcel)
+    {
+        //Implement
+    }
+
+    private void HandleSuccessfulDelivery(Parcel parcel)
+    {
+        //Implement
+    }
+    
+    public Report GetReport()
+    {
+        //Implement
+    }
+}
