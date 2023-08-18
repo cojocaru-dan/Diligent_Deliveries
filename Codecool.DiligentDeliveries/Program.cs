36e11448757502adb3ebdea84cef0ea2d226e6e3
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-08-18 11:15:40 +0300

    Implementing classes and create a delivery app

--- Codecool.DiligentDeliveries/Program.cs
+++ Codecool.DiligentDeliveries/Program.cs
@@ -6,11 +6,86 @@ class Program
 
     private static string GetRandomStreetAddress() =>
         $"Random Street{Random.Next(100, 1000)} Number {Random.Next(0, 100)}";
-    
+
+    private static int GetRandomZipCode() => Random.Next(1000, 10000);
+    private static string GetRandomCustomerName()
+    {
+        string[] customerNames = 
+        {
+            "Emily Johnson", "Liam Anderson", "Olivia Williams", "Noah Smith", "Ava Brown",
+            "Ethan Davis", "Isabella Martinez", "Aiden Taylor", "Sophia Anderson", "Jackson Robinson",
+            "Mia Harris", "Lucas Jackson", "Harper Thompson", "Oliver Davis", "Amelia Martinez",
+            "Elijah Wilson", "Charlotte Moore", "Grayson White", "Abigail Turner", "Carter Scott",
+            "Evelyn Hall", "Sebastian Walker", "Elizabeth Martinez", "Alexander Allen", "Scarlett Lewis",
+            "James Foster", "Luna Carter", "Benjamin Mitchell", "Victoria Green", "Henry Wright",
+            "Lily Murphy", "Samuel Hall", "Grace Miller", "Daniel Baker", "Chloe Adams",
+            "Matthew Hill", "Zoey Clark", "Joseph Johnson", "Penelope Mitchell", "Michael Turner"
+        };
+
+        return customerNames[Random.Next(customerNames.Length)];
+    }
+
+    private static List<Parcel> GenerateListOfParcels(int packages)
+    {
+        List<Parcel> parcels = new List<Parcel>();
+        for (int i = 0; i < packages; i++)
+        {   
+
+            Address addr = new Address(GetRandomZipCode(), GetRandomStreetAddress(), GetRandomCustomerName());
+            Parcel parcel = new Parcel(i, addr);
+
+            parcels.Add(parcel);
+        }
+        return parcels;
+    }
 
     public static void Main(string[] args)
     {
-        const int packages = 10;
-        const int maximumReattempts = 5;
+        const int packages = 20;
+        const int maximumReattempts = 3;
+
+        List<Rider> riders = new List<Rider>();
+        int[] reattemptsPerAddress = { 1, 2, 5 };
+
+        StartRidersRoutine(riders, reattemptsPerAddress, maximumReattempts, packages);
+        GetFinalReport(riders);
+    }
+
+    private static void StartRidersRoutine(List<Rider> riders, int[] reattemptsPerAddress, int maximumReattempts, int packages)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            List<Parcel> parcels = GenerateListOfParcels(packages);
+            Rider rider = new Rider(i, reattemptsPerAddress[i], maximumReattempts, packages);
+            rider.AddParcels(parcels);
+            rider.StartRoutine();
+            riders.Add(rider);
+        }
+    }
+
+    private static void GetFinalReport(List<Rider> riders)
+    {
+        int maxSuccessfullyDeliveries = -1;
+        List<Rider> winners = new List<Rider>();
+
+        foreach (var rider in riders)
+        {
+            if (rider.SuccessfullDeliveries > maxSuccessfullyDeliveries)
+            {
+                maxSuccessfullyDeliveries = rider.SuccessfullDeliveries;
+                winners.Clear();
+                winners.Add(rider);
+            }
+            else if (rider.SuccessfullDeliveries == maxSuccessfullyDeliveries)
+            {
+                winners.Add(rider);
+            }
+            Console.WriteLine(rider.GetReport());
+        }
+        Console.WriteLine($"We have {winners.Count} winner{(winners.Count == 1 ? "" : "s")}");
+        foreach (var winner in winners)
+        {
+            Console.WriteLine($"The winner is {winner.Name}");
+        }
     }
 }
--- Codecool.DiligentDeliveries/Rider.cs
+++ Codecool.DiligentDeliveries/Rider.cs
@@ -4,34 +4,161 @@ public class Rider
 {
     public int Id { get; }
     public string Name => $"Rider{Id}";
+    public int ReattemptsPerAddress { get; }
+    public int MaximumReattempts { get; }
+    public int ReattemptsLeft { get; private set; }
+    public int SuccessfullDeliveries { get; private set; }
+    public List<Parcel> PackagesList { get; private set; }
+    public int NrOfPackages { get; }
 
-    public Rider(int id, int reattemptPerAddress, int maximumReattempts)
+
+    public Rider(int id, int reattemptPerAddress, int maximumReattempts, int packages)
     {
         //Initialize members
+        Id = id;
+        ReattemptsPerAddress = reattemptPerAddress;
+        MaximumReattempts = maximumReattempts;
+        ReattemptsLeft = maximumReattempts;
+        SuccessfullDeliveries = 0;
+        PackagesList = new List<Parcel>();
+        NrOfPackages = packages;
     }
 
     public void AddParcels(List<Parcel> parcels)
     {
         //Implement
+        PackagesList.Clear();
+        PackagesList.AddRange(parcels);
     }
 
     public void StartRoutine()
     {
         //Implement
+        foreach (var parcel in PackagesList)
+        {
+            if (parcel.Deliver())
+            {
+                HandleSuccessfulDelivery(parcel);   
+            };
+        }
+
+        foreach (var parcel in PackagesList)
+        {
+            for (int i = 0; i < ReattemptsPerAddress; i++)
+            {
+                if (!parcel.Delivered && ReattemptsLeft > 0) 
+                {
+                    if (Reattempt(parcel))
+                    {
+                        HandleSuccessfulDelivery(parcel);
+                    }
+                    
+                }
+            }
+        }       
     }
 
-    private bool Reattempt(Parcel parcel)
+    private bool Reattempt(Parcel parcel) 
     {
-        //Implement
+        //Implement 
+        ReattemptsLeft--;
+        return parcel.Deliver();
     }
 
     private void HandleSuccessfulDelivery(Parcel parcel)
     {
         //Implement
+        parcel.Delivered = true;
+        SuccessfullDeliveries++;
     }
     
     public Report GetReport()
     {
         //Implement
+        return new Report(Name, SuccessfullDeliveries, NrOfPackages, ReattemptsLeft);
+    }
+}
+
+public class Address
+{
+    public int ZipCode { get; }
+    public string StreetAddress { get; }
+    public string CustomerName { get; }
+
+    public Address(int zipcode, string streetAddress, string customerName)
+    {
+        ZipCode = zipcode;
+        StreetAddress = streetAddress;
+        CustomerName = customerName;
+    }
+    public bool Equals(Address? obj)
+    {
+        if (obj == null || obj.GetType() != this.GetType()) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (ZipCode == obj.ZipCode && StreetAddress == obj.StreetAddress && CustomerName == obj.CustomerName) return true;
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return ZipCode;
+    }
+
+    public override string ToString()
+    {
+        return $"ZipCode: {ZipCode}, StreetAddress: {StreetAddress}, CustomerName: {CustomerName}";
+    } 
+}
+
+public class Parcel
+{
+    private static Random rand = new Random();
+    public int Id { get; }
+    public Address Address { get; set; }
+
+    public bool Delivered { get; set; }
+
+    public Parcel(int id, Address address)
+    {
+        Id = id;
+        Address = address;
+        Delivered = false;
+    }
+
+    public bool Deliver()
+    {
+        int[] ints= new int[100];
+
+        for (int i = 0; i < 50; i++)
+        {
+            ints[i] = 1;
+            ints[i + 50] = rand.Next(1, 8);
+        }
+
+        return ints[rand.Next(0, ints.Length)] == 1;
+    }
+
+    public override string ToString()
+    {
+        return $"Parcel_Id: {Id}, Parcel_Address: {Address}";
+    }
+}
+
+public class Report
+{
+    public string Name { get; }
+    public int SuccessfullDeliveries { get; }
+    public int NrOfPackages { get; }
+    public int ReattemptsLeft { get; }
+
+    public Report(string name, int successfullDeliveries, int nrOfPackages, int reattemptsLeft)
+    {
+        Name = name;
+        SuccessfullDeliveries = successfullDeliveries;
+        NrOfPackages = nrOfPackages;
+        ReattemptsLeft = reattemptsLeft;
+    }
+    public override string ToString()
+    {
+        return $"{Name} delivered {SuccessfullDeliveries} packages out of {NrOfPackages} ({ReattemptsLeft} reattempts left)";
     }
 }
